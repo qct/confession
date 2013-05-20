c467cd305fec549fa5824031e052220172789ec6
qct-office <quchentao@163.com>
2013-05-20 16:18:40 +0800

    google oauth2

--- utils.js
+++
@@ -1,7 +0,0 @@
-var moment = require('moment');
-moment.lang('zh-cn');
-this.add_formatted_datestr = function (posts) {
-  for (var i = 0; i < posts.length; i++) {
-    posts[i].updated_at_str = moment(posts[i].updated_at.getTime()).fromNow();//.format("YYYY-MM-DD HH:mm:ss");
-  }
-}
